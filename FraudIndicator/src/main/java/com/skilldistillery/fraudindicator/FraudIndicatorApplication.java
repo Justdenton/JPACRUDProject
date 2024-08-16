d7619cbfb445760e14919e249de0e6e79b9b8861
“Justin” <jdenton.bills@gmail.com>
2024-08-16 17:44:20 -0500

    Create Boot MVC project, connect JPA project dependency

---
+++ FraudIndicator/src/main/java/com/skilldistillery/fraudindicator/FraudIndicatorApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fraudindicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudIndicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudIndicatorApplication.class, args);
+	}
+
+}
